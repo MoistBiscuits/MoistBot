403f623022bbfc9f3b6d1052971dbdede7c1f76d
MoistBiscuits <aqeel.little@hotmail.co.uk>
2021-06-17 11:55:24 +0100

    Added simple resoponse on any message that contains 'Moist' in chat

--- bot.py
+++ bot.py
@@ -2,17 +2,36 @@
 import os
 
 import discord
+import random
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
-print(TOKEN)
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    moist_responses = ["Lovely choice of vocabulary",
+		"Wow that is really interesting",
+		"I like this guy",
+		"Whatever you said, I agree!"]
+
+    if 'moist' in message.content.lower():
+        response = random.choice(moist_responses)
+        await message.channel.send(response)
 
 client.run(TOKEN)
