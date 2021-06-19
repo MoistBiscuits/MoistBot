e9b803d2766e156103e97fb614354b2aa5cb9eb1
Aqeel Little <65546472+MoistBiscuits@users.noreply.github.com>
2021-06-19 11:48:58 +0100

    Update bot.py
    Added parameters to !random to give an upper and lower bound

--- bot.py
+++ bot.py
@@ -9,6 +9,40 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
+<<<<<<< HEAD
+#Gives a rabdom number between 2 given values
+#If only 1 arguement is passed, from 0 to X
+#if none, from 0 to 10
+@bot.command(name='random', help='Gives a random number between 2 values')
+async def random_number(ctx, *args):
+    lower = 0
+    upper = 10
+
+    if len(args) > 2:
+        ctx.send("Invalid number of arguements, expected 0 to 2")
+        return
+
+    for val in args:
+        if (val.isdecimal == False):
+            ctx.send("Arguements must be postive integers")
+            return
+
+
+    if len(args) == 2:
+        upper = int(args[1])
+        lower = int(args[0])
+    elif len(args) == 1:
+        upper = int(args[0])
+
+    response = random.randrange(lower,upper)
+    await ctx.send(response)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+=======
 @bot.command(name='random')
 async def nine_nine(ctx):
     response = random.randrange(0,10)
@@ -19,6 +53,7 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
+>>>>>>> 8ddff542a4d7c9c5d44c19ba103ab165e36755e9
     moist_responses = ["Lovely choice of vocabulary",
 		"Wow that is really interesting",
 		"I like this guy",
