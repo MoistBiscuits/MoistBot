413e2627c5dec604f1d9774d3a6eb9967058c945
MoistBiscuits <aqeel.little@hotmail.co.uk>
2021-06-19 11:30:59 +0100

    Improved !random to accept parameters for range

--- bot.py
+++ bot.py
@@ -9,9 +9,31 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name='random')
-async def nine_nine(ctx):
-    response = random.randrange(0,10)
+#Gives a rabdom number between 2 given values
+#If only 1 arguement is passed, from 0 to X
+#if none, from 0 to 10
+@bot.command(name='random', help='Gives a random number between 2 values')
+async def random_number(ctx, *args):
+    lower = 0
+    upper = 10
+
+    if len(args) > 2:
+        ctx.send("Invalid number of arguements, expected 0 to 2")
+        return
+
+    for val in args:
+        if (val.isdecimal == False):
+            ctx.send("Arguements must be postive integers")
+            return
+
+
+    if len(args) == 2:
+        upper = int(args[1])
+        lower = int(args[0])
+    elif len(args) == 1:
+        upper = int(args[0])
+
+    response = random.randrange(lower,upper)
     await ctx.send(response)
 
 @bot.event
